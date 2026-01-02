1ab484d4ef7d66ec88e013a3d71d9967bfab21c5
pawank06 <pawan06kumar2003@gmail.com>
2026-01-02 22:41:43 +0530

    feature(type): extend the type builtin to search for executable files using PATH

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::io::{self, Write};
+use std::env;
+use std::path::Path;
+use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
 fn main() {
     loop {    
@@ -33,7 +37,36 @@ fn main() {
             if builtin.contains(query) {
                 println!("{} is a rshell builtin", query);
             } else {
-                println!("{} not found", query);
+                match env::var("PATH") {
+                    Ok(val) => {
+                       let paths: Vec<&str> = val.split(":").collect();
+                       let mut found = false;
+                       for dir in paths {
+                           let full_path = Path::new(dir).join(query);
+                           if !full_path.exists() {
+                               continue;
+                           }
+                           
+                           let metadata = match fs::metadata(&full_path) {
+                               Ok(m) => m,
+                               Err(_) => continue
+                           };
+                           
+                           let mode = metadata.permissions().mode();
+                           
+                           if mode & 0o11 != 0 {
+                               println!("{} is {}", query, full_path.display());
+                               found = true;
+                               break;
+                           }                          
+                       }
+                       
+                       if !found {
+                           println!("{}: not found", query);
+                       }
+                    },
+                    Err(e) => println!("Couldn't find val {}", e)
+                }
             }
         },
         _ =>  println!("{}: command not found", input.trim()),
