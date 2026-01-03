96ed79adac0a9918e5cc9139b7604360827efb8a
pawan <pawan06kumar2003@gmail.com>
2026-01-03 23:19:00 +0530

    feat(execute): add support for running external programs with arguments (#7)
    * feat(execute): add support for running external programs with arguments
    
    * fix(warning): the borrowed expression implements the required traits
    
    * fix(lint): fix code linting
    
    * fix(typo): fix error message typo
    
    * fix(unwrap): handle unwrap

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::env;
 use std::path::Path;
 use std::fs;
 use std::os::unix::fs::PermissionsExt;
+use std::process::Command;
 
 fn main() {
     loop {    
@@ -20,7 +21,9 @@ fn main() {
         continue;
     }
     
-    match parts[0] {
+    let command = parts[0];
+    
+    match command {
         "exit" => break,
         "echo" => {
             let args = &parts[1..];
@@ -30,46 +33,91 @@ fn main() {
             if parts.len() < 2 {
                 continue;
             }
+            let query = &parts[1];
             
             let builtin = ["exit", "echo", "type"];
-            let query = &parts[1];
             
             if builtin.contains(query) {
                 println!("{} is a rshell builtin", query);
             } else {
                 match env::var("PATH") {
                     Ok(val) => {
-                       let paths: Vec<&str> = val.split(":").collect();
-                       let mut found = false;
-                       for dir in paths {
-                           let full_path = Path::new(dir).join(query);
-                           if !full_path.exists() {
-                               continue;
-                           }
+                        let mut found = false;
+                        for dir in val.split(":") {
+                            let full_path = Path::new(dir).join(query);
+                            if !full_path.exists() {
+                                continue;
+                            }
                            
-                           let metadata = match fs::metadata(&full_path) {
-                               Ok(m) => m,
-                               Err(_) => continue
-                           };
-                           
-                           let mode = metadata.permissions().mode();
-                           
-                           if mode & 0o111 != 0 {
-                               println!("{} is {}", query, full_path.display());
-                               found = true;
-                               break;
-                           }                          
-                       }
+                            let metadata = match fs::metadata(&full_path) {
+                                Ok(m) => m,
+                                Err(_) => continue
+                            };
+                            
+                            let mode = metadata.permissions().mode();
+                            
+                            if mode & 0o111 != 0 {
+                                println!("{} is {}", query, full_path.display());
+                                found = true;
+                                break;
+                            }                          
+                        }
                        
-                       if !found {
-                           println!("{}: not found", query);
-                       }
+                        if !found {
+                            println!("{}: not found", query);
+                        }
                     },
                     Err(e) => println!("Couldn't find val {}", e)
                 }
             }
         },
-        _ =>  println!("{}: command not found", input.trim()),
+        _ =>  {
+            if command.contains("/") {
+                match Command::new(command)
+                    .args(&parts[1..])
+                    .status() {
+                        Ok(_) => {},
+                        Err(e) => eprintln!("{}: {}", command, e),
+                    };
+                
+                    continue;
+            }
+            
+            match env::var("PATH") {
+                Ok(val) => {
+                    let mut found = false;
+                   for dir in val.split(":") {
+                       let full_path = Path::new(&dir).join(command);
+                       
+                       if !full_path.exists() {
+                           continue;
+                       }
+                       
+                       let metadata = match fs::metadata(&full_path) {
+                           Ok(m) => m,
+                           Err(_) => continue
+                       };
+                       
+                       let mode = metadata.permissions().mode();
+                       
+                       if mode & 0o111 != 0 {
+                           match Command::new(full_path)
+                               .args(&parts[1..])
+                               .status() {
+                                   Ok(_) => {},
+                                   Err(err) => eprintln!("{}: {}", command, err),
+                               }
+                           found = true;
+                           break;
+                       }
+                   } 
+                   if !found {
+                       println!("{}: command not found", command);
+                   }
+                },
+                Err(e) => println!("Couldn't find val {}", e)
+            }
+        }
     }
     
     }
