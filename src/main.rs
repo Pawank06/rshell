6e47183534a3c6d4216f8cbca581fc10125992a2
pawank06 <pawan06kumar2003@gmail.com>
2026-01-06 21:04:24 +0530

    clippy changes

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,128 @@
-use std::io::{self, Write};
 use std::env;
+use std::fs;
+use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
 use std::os::unix::process::CommandExt;
 use std::path::Path;
-use std::fs;
-use std::os::unix::fs::PermissionsExt;
 use std::process::Command;
 
 fn main() {
-    loop {    
-    print!("$ ");
-    io::stdout()
-        .flush().unwrap();
-    
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input).expect("unable to read line");
-    
-    let parts: Vec<&str> = input.split_whitespace().collect();
-    
-    if parts.is_empty() {
-        continue;
-    }
-    
-    let command = parts[0];
-    
-    match command {
-        "exit" => break,
-        "echo" => {
-            let args = &parts[1..];
-            println!("{}", args.join(" "));
-        },
-        "type" => {
-            if parts.len() < 2 {
-                continue;
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("unable to read line");
+
+        let parts: Vec<&str> = input.split_whitespace().collect();
+
+        if parts.is_empty() {
+            continue;
+        }
+
+        let command = parts[0];
+
+        match command {
+            "exit" => break,
+            "echo" => {
+                let args = &parts[1..];
+                println!("{}", args.join(" "));
             }
-            let query = &parts[1];
-            
-            let builtin = ["exit", "echo", "type", "pwd"];
-            
-            if builtin.contains(query) {
-                println!("{} is a rshell builtin", query);
-            } else {
+            "type" => {
+                if parts.len() < 2 {
+                    continue;
+                }
+                let query = &parts[1];
+
+                let builtin = ["exit", "echo", "type", "pwd"];
+
+                if builtin.contains(query) {
+                    println!("{} is a rshell builtin", query);
+                } else {
+                    match env::var("PATH") {
+                        Ok(val) => {
+                            let mut found = false;
+                            for dir in val.split(":") {
+                                let full_path = Path::new(dir).join(query);
+                                if !full_path.exists() {
+                                    continue;
+                                }
+
+                                let metadata = match fs::metadata(&full_path) {
+                                    Ok(m) => m,
+                                    Err(_) => continue,
+                                };
+
+                                let mode = metadata.permissions().mode();
+
+                                if mode & 0o111 != 0 {
+                                    println!("{} is {}", query, full_path.display());
+                                    found = true;
+                                    break;
+                                }
+                            }
+
+                            if !found {
+                                println!("{}: not found", query);
+                            }
+                        }
+                        Err(e) => eprintln!("couldn't read PATH environment variable: {}", e),
+                    }
+                }
+            }
+            "pwd" => match env::current_dir() {
+                Ok(val) => println!("{}", val.display()),
+                Err(e) => eprintln!("pwd: {}", e),
+            },
+            _ => {
+                if command.contains("/") {
+                    match Command::new(command).args(&parts[1..]).status() {
+                        Ok(_) => {}
+                        Err(e) => eprintln!("{}: {}", command, e),
+                    };
+
+                    continue;
+                }
+
                 match env::var("PATH") {
                     Ok(val) => {
                         let mut found = false;
                         for dir in val.split(":") {
-                            let full_path = Path::new(dir).join(query);
+                            let full_path = Path::new(&dir).join(command);
+
                             if !full_path.exists() {
                                 continue;
                             }
-                           
+
                             let metadata = match fs::metadata(&full_path) {
                                 Ok(m) => m,
-                                Err(_) => continue
+                                Err(_) => continue,
                             };
-                            
+
                             let mode = metadata.permissions().mode();
-                            
+
                             if mode & 0o111 != 0 {
-                                println!("{} is {}", query, full_path.display());
+                                match Command::new(&full_path)
+                                    .arg0(command)
+                                    .args(&parts[1..])
+                                    .status()
+                                {
+                                    Ok(_) => {}
+                                    Err(err) => eprintln!("{}: {}", command, err),
+                                }
                                 found = true;
                                 break;
-                            }                          
+                            }
                         }
-                       
                         if !found {
-                            println!("{}: not found", query);
+                            println!("{}: command not found", command);
                         }
-                    },
-                    Err(e) => eprintln!("couldn't read PATH environment variable: {}", e)
+                    }
+                    Err(e) => println!("couldn't find val {}", e),
                 }
             }
-        },
-        "pwd" => {
-            match env::current_dir(){
-              Ok(val) => println!("{}", val.display()),
-              Err(e) => eprintln!("pwd: {}", e),
-          }
-          
-        },
-        _ =>  {
-            if command.contains("/") {
-                match Command::new(command)
-                    .args(&parts[1..])
-                    .status() {
-                        Ok(_) => {},
-                        Err(e) => eprintln!("{}: {}", command, e),
-                    };
-                
-                    continue;
-            }
-            
-            match env::var("PATH") {
-                Ok(val) => {
-                    let mut found = false;
-                   for dir in val.split(":") {
-                       let full_path = Path::new(&dir).join(command);
-                       
-                       if !full_path.exists() {
-                           continue;
-                       }
-                       
-                       let metadata = match fs::metadata(&full_path) {
-                           Ok(m) => m,
-                           Err(_) => continue
-                       };
-                       
-                       let mode = metadata.permissions().mode();
-                       
-                       if mode & 0o111 != 0 {
-                           match Command::new(&full_path)
-                               .arg0(command)
-                               .args(&parts[1..])
-                               .status() {
-                                   Ok(_) => {},
-                                   Err(err) => eprintln!("{}: {}", command, err),
-                               }
-                           found = true;
-                           break;
-                       }
-                   } 
-                   if !found {
-                       println!("{}: command not found", command);
-                   }
-                },
-                Err(e) => println!("couldn't find val {}", e)
-            }
         }
-    }
-    
     }
 }
